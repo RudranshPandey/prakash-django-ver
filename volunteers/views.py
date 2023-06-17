be7eb0fdd5cefdaa589cb95d2cce1db422d79f0d
RudranshPandey <99203088+RudranshPandey@users.noreply.github.com>
2023-06-17 23:08:46 +0530

    This is working latest commit

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .forms import volunteer_profiles, volunteerform
 from .models import volunteer_profiles
-
+from django.core.paginator import Paginator
 
 
 def addvolunteer(request):
@@ -15,8 +15,11 @@ def addvolunteer(request):
     return render(request,"volunteers/add.html",{"form":form})
 
 def index(request):
-    volunteers = volunteer_profiles.objects.all().order_by('-volunteer_no')
-    return render(request,"volunteers/index.html",{"volunteers":volunteers})
+    volunteers = volunteer_profiles.objects.all().order_by('-id')
+    q = Paginator(volunteer_profiles.objects.all(),10)
+    page = request.GET.get('page')
+    vols = q.get_page(page)
+    return render(request,"volunteers/index.html",{"volunteers":volunteers,'vols':vols})
 
 def update_view(request,pk):
     object = get_object_or_404(volunteer_profiles,pk=pk)  # Use the passed pk argument instead of hardcoding it
