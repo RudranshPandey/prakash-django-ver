24b0df21f19043ca3d0401c71bf9cb40bb3b5056
Rudransh Pandey <99203088+RudranshPandey@users.noreply.github.com>
2023-06-21 22:05:00 +0530

    Delete victims directory

--- victims/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VictimsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'victims'
--- victims/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import All_profiles
-class All_profilesSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = All_profiles
-        fields = '__all__'
